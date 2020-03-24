b9bd92d2aefb696008ec68c1620ca8e108ef88b4
Marco Roberti <m.roberti@unito.it>
2020-03-24 22:04:50 +0100

    bugfix

--- data/run_ner.py
+++ data/run_ner.py
@@ -709,10 +709,10 @@ def main():
                 for line in f:
                     if line.startswith("-DOCSTART-") or line == "" or line == "\n":
                         writer.write(line)
-                        if not predictions[example_id]:
+                        if not predictions[0][example_id]:
                             example_id += 1
-                    elif predictions[example_id]:
-                        output_line = line.split()[0] + " " + predictions[example_id].pop(0) + "\n"
+                    elif predictions[0][example_id]:
+                        output_line = line.split()[0] + " " + ' '.join([p[example_id].pop(0) for p in predictions]) + "\n"
                         writer.write(output_line)
                     else:
                         logger.warning("Maximum sequence length exceeded: No prediction for '%s'.", line.split()[0])
