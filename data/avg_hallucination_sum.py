404f6ff627707028bc6b9953a8a9bd297a7a9b4f
KaijuML <clement.rebuffel@lip6.fr>
2020-05-04 11:06:18 +0200

    using TaggedFileIterable

--- data/avg_hallucination_sum.py
+++ data/avg_hallucination_sum.py
@@ -1,4 +1,5 @@
 import argparse
+from utils import TaggedFileIterable
 from statistics import mean
 
 if __name__ == '__main__':
@@ -7,16 +8,20 @@ if __name__ == '__main__':
                                      'each one along with its hallucination score. '
                                      'Sentences are separated by an empty line.')
     args = parser.parse_args()
-
-    scores = []
-    accumulator = 0
-    with open(args.file) as f:
-        for line in f:
-            if line.strip():
-                accumulator += float(line.strip().split()[1])
-            else:
-                scores.append(accumulator)
-                accumulator = 0
-
-    avg_score = mean(scores)
+    
+    tagged_hypotheses = TaggedFileIterable.from_filename(args.file, func=lambda x,y: float(y))
+    avg_score = mean([sum(h) for h in tagged_hypotheses])
     print(f'{args.file}:\t{avg_score:.4f}')
+
+    # scores = []
+    # accumulator = 0
+    # with open(args.file) as f:
+    #     for line in f:
+    #         if line.strip():
+    #             accumulator += float(line.strip().split()[1])
+    #         else:
+    #             scores.append(accumulator)
+    #             accumulator = 0
+
+    # avg_score = mean(scores)
+    # print(f'{args.file}:\t{avg_score:.4f}')
