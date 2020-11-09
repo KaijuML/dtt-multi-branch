08f3adcc31d3fe9b734ea863b4cd6a1d737d2539
marco\-roberti <m.roberti@unito.it>
2020-11-09 13:02:35 +0100

    reversed references/hypotheses

--- ttest_ngram_metrics.py
+++ ttest_ngram_metrics.py
@@ -16,23 +16,23 @@ def t_test(packed):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compute instance-level BLEU and PARENT metrics for two different '
-                                                 'reference files. For each metric, perform a t-test over the two '
+                                                 'hypothesis files. For each metric, perform a t-test over the two '
                                                  'files.')
     parser.add_argument('tables')
-    parser.add_argument('references_1')
-    parser.add_argument('references_2')
-    parser.add_argument('hypotheses')
+    parser.add_argument('references')
+    parser.add_argument('hypotheses_1')
+    parser.add_argument('hypotheses_2')
 
     args = parser.parse_args()
 
     tables = FileIterable.from_filename(args.tables, fmt='jl')
-    references = [FileIterable.from_filename(args.references_1, fmt='txt'),
-                  FileIterable.from_filename(args.references_2, fmt='txt')]
-    hypotheses = FileIterable.from_filename(args.hypotheses, fmt='txt')
+    references = FileIterable.from_filename(args.references, fmt='txt')
+    hypotheses = [FileIterable.from_filename(args.hypotheses_1, fmt='txt'),
+                  FileIterable.from_filename(args.hypotheses_2, fmt='txt')]
 
-    bleus = tuple([sentence_bleu([r], h) for r, h in zip(refs, hypotheses)] for refs in references)
-    parents = np.array([[parent_instance_level((h, r, t)) for h, r, t in zip(hypotheses, refs, tables)]
-                        for refs in references])
+    bleus = tuple([sentence_bleu([r], h) for r, h in zip(references, hyps)] for hyps in hypotheses)
+    parents = np.array([[parent_instance_level((h, r, t)) for h, r, t in zip(hyps, references, tables)]
+                        for hyps in hypotheses])
 
     t_tests = {
         'bleu': t_test(bleus),
